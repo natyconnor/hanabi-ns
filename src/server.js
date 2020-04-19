1fa257b8b0b7a780b132ef32f3eca59b633da760
nathanc <nathanc@hotpads.com>
2020-04-19 11:59:31 -0700

    TicTacToe example now working

---
+++ src/server.js
@@ -0,0 +1,25 @@
+import { Server } from 'boardgame.io/server';
+import path from 'path';
+import Koa from 'koa';
+import serve from 'koa-static';
+import mount from 'koa-mount';
+import { TicTacToe } from './game';
+
+const server = Server({ games: [TicTacToe] });
+const PORT = process.env.PORT || 8000;
+
+// Build path relative to the server.js file
+const frontEndAppBuildPath = path.resolve(__dirname, './build');
+
+// Serve the build directory
+const static_pages = new Koa();
+static_pages.use(serve(frontEndAppBuildPath));
+server.app.use(mount('/', static_pages));
+server.run(PORT, () => {
+    server.app.use(
+        async (ctx, next) => await serve(frontEndAppBuildPath)(
+            Object.assign(ctx, { path: 'index.html' }),
+            next
+        )
+    )
+});
